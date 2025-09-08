398515dc7d5cffe5c8251dcb78071c28d6e1494b
Colour1205 <me@uaena.io>
2025-09-08 11:35:28 -0400

    refactored to extract helper method

--- src/lab1/FizzBuzz.java
+++ src/lab1/FizzBuzz.java
@@ -3,34 +3,27 @@ package lab1;
 /**
  * Solve the lab1.FizzBuzz challenge.
  */
-class FizzBuzz {
+public class FizzBuzz {
 
     public static void main(String[] args) {
+        extracted();
+    }
 
+    private static void extracted() {
         for (int i = 1; i < 100; i++) {
+            System.out.println(getFizzBuzz(i));
+        }
+    }
 
-            // Find out which numbers divide i.
-            boolean divisibleBy3 = i % 3 == 0;
-            boolean divisibleBy5 = i % 5 == 0;
-
-            // Print our appropriate result.
-            if (divisibleBy3 && divisibleBy5) {
-
-                System.out.println("Fizz Buzz");
-
-            } else if (divisibleBy3) {
-
-                System.out.println("Fizz");
-
-            } else if (divisibleBy5) {
-
-                System.out.println("Buzz");
-
-            } else {
-
-                System.out.println(i);
-
-            }
+    private static String getFizzBuzz(int number) {
+        if (number % 15 == 0) {
+            return "Fizz Buzz";
+        } else if (number % 3 == 0) {
+            return "Fizz";
+        } else if (number % 5 == 0) {
+            return "Buzz";
+        } else {
+            return String.valueOf(number);
         }
     }
 }
